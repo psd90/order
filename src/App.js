d6734bdbae562903a8a85c77eca8ce6e6eebdde7
psd90 <pdhillon.dev@gmail.com>
2021-01-14 17:25:59 +0000

    added logo and link to facebook

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pre-ordering food Site
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/Dhillonscurry"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Back to Facebook
         </a>
       </header>
     </div>
