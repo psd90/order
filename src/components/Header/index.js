b173e54798e647543d18331e754be9b9e297968a
psd90 <pdhillon.dev@gmail.com>
2021-01-22 18:07:43 +0000

    react router and some styling with layouts with a simple footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 import Logo from './../../assets/Logo.png'; 
 
@@ -7,7 +8,18 @@ const Header = props => {
         <header className="header">
             <div className="wrap">
                 <div className="logo">
+                    <Link to="/">
                     <img src={Logo} alt="Dhillons"/>
+                    </Link>
+                </div>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                            Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
