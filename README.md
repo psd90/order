da245609848843badfb4819dd960d6abdea1a60a
psd90 <pdhillon.dev@gmail.com>
2021-01-22 12:30:34 +0000

    fresh template

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Creating a simple food ordering site/app to prebook food. 
 The chef should be able to easily edit the menu.
+There will be no payment as it is only to prebook and
+inform the chef how much they need to prepare for the customer.
+I am going to use React, Firebase for admin/user and 
+I will use Redux to learn more about state management 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Pre-ordering food Site
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://www.facebook.com/Dhillonscurry"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Back to Facebook
-        </a>
-      </header>
+      <h2>Hi</h2>
     </div>
   );
 }
