92cac1163b605ab6249dbf009938c80b221cb763
psd90 <pdhillon.dev@gmail.com>
2021-01-22 16:44:39 +0000

    landing page with components built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import Header from './components/Header'
+import Homepage from './pages/Homepage'
+import './default.scss';
 
 
 function App() {
   return (
     <div className="App">
-      <h2>Hi</h2>
+      <Header />
+      <div className="main">
+      <Homepage />
+      </div>
     </div>
   );
 }
