9c4b9fc6c1d3190bd751153da2d3d53b886921e1
psd90 <pdhillon.dev@gmail.com>
2021-03-19 11:51:42 +0000

    date selector is added and admin can change the cut off date

--- src/components/DateEditor/index.js
+++ src/components/DateEditor/index.js
@@ -1,28 +1,53 @@
-import React, {useEffect} from 'react';
-import {firestore} from '../../firebase/util'
+import React, {useState} from 'react';
+import {firestore} from '../../firebase/util';
+import DatePicker from 'react-datepicker';
+import Button from './../../components/Forms/Button';
+import 'react-datepicker/dist/react-datepicker.css';
 import './styles.scss';
+import {useHistory} from 'react-router-dom';
 
-
+//date selector will need manually setting up 
 
 const DateEditor = () => {
+    const [selectedDate, setSelectedDate] = useState(null)
+    const history = useHistory();
 
-
-// useEffect(()=> {
-//     firestore.collection('date').doc().set()
-// })
-
-
+ // in here make the doc reference applied to the one doc('in here')
+const handleSubmit = e => {
+    e.preventDefault();
+    firestore.collection('date').doc('1KXlQPbP2cxAMiTurXXf').set({date: selectedDate})
+}
 
 return(
     <div>
         <p>
-            Make a date selector to push a new cut-off date to firebase 'date' collection deleting the current cut off date
+            Click on the box below to pick a cut off date 
         </p>
-        <br></br>
-        <p>
-           When posting a new Cut Off Date it will delete all orders
-        </p>
+    <div className="selectDate">
+        <form onSubmit={handleSubmit}>
+
+        <DatePicker 
+        selected={selectedDate} 
+        onChange={date => setSelectedDate(date)} 
+        dateFormat = 'dd/MM/yyyy'
+        minDate ={new Date}
+        isClearable
+        />
+        <Button type="submit">
+              Set Date
+            </Button>
+            <br></br>
+            <Button onClick={() => history.push('/')}>
+                View New Date
+            </Button>
+        </form>
+    </div>
+    
+
+
     </div>
 )
 }
+
+
 export default DateEditor
